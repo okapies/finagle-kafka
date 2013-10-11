71b162f269c623768fb45835e2541496fc3b0d22
okapies <okapies@gmail.com>
2013-10-11 23:27:30 +0900

    add tests for message that has no key

---
+++ src/test/scala/okapies/finagle/kafka/protocol/MessageSetTest.scala
@@ -0,0 +1,12 @@
+package okapies.finagle.kafka.protocol
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: okapy
+ * Date: 2013/10/11
+ * Time: 23:19
+ * To change this template use File | Settings | File Templates.
+ */
+class MessageSetTest {
+
+}
--- src/test/scala/okapies/finagle/kafka/protocol/MessageTest.scala
+++ src/test/scala/okapies/finagle/kafka/protocol/MessageTest.scala
@@ -6,12 +6,9 @@ import org.scalatest.matchers._
 
 import org.jboss.netty.buffer.ChannelBuffers
 
-import util.GatheringByteChannelMock
-
 class MessageTest extends FlatSpec with ShouldMatchers {
 
   import kafka.message.{
-    ByteBufferMessageSet,
     Message => KafkaMessage,
     NoCompressionCodec
   }
@@ -30,8 +27,21 @@ class MessageTest extends FlatSpec with ShouldMatchers {
     assert(kafkaMsg1.checksum === msg1.crc)
     assert(kafkaMsg1.magic === msg1.magicByte)
     assert(kafkaMsg1.attributes === msg1.attributes)
-    assert(kafkaMsg1.key.asString === msg1.key.get.toString(utf8))
-    assert(kafkaMsg1.payload.asString === msg1.value.toString(utf8))
+    assert(kafkaMsg1.key.asString === "key1")
+    assert(kafkaMsg1.payload.asString === "value1")
+
+    val msg2 = Message.create(
+      ChannelBuffers.wrappedBuffer("value2".getBytes(utf8)),     // value
+      None,                                                      // key
+      0                                                          // codec
+    )
+    val kafkaMsg2 = new KafkaMessage(msg2.underlying.toByteBuffer)
+
+    assert(kafkaMsg2.checksum === msg2.crc)
+    assert(kafkaMsg2.magic === msg2.magicByte)
+    assert(kafkaMsg2.attributes === msg2.attributes)
+    assert(kafkaMsg2.key === null)
+    assert(kafkaMsg2.payload.asString === "value2")
   }
 
   it should "decode a no compressed message" in {
@@ -45,12 +55,20 @@ class MessageTest extends FlatSpec with ShouldMatchers {
     assert(msg1.crc === kafkaMsg1.checksum)
     assert(msg1.magicByte === kafkaMsg1.magic)
     assert(msg1.attributes === kafkaMsg1.attributes)
-    assert(msg1.key.get.toString(utf8) === kafkaMsg1.key.asString)
-    assert(msg1.value.toString(utf8) === kafkaMsg1.payload.asString)
+    assert(msg1.key.get.toString(utf8) === "key1")
+    assert(msg1.value.toString(utf8) === "value1")
+
+    val kafkaMsg2 = new KafkaMessage(
+      "value2".getBytes(utf8), // value
+      NoCompressionCodec       // codec
+    )
+    val msg2 = Message(ChannelBuffers.wrappedBuffer(kafkaMsg2.buffer))
+
+    assert(msg2.crc === kafkaMsg2.checksum)
+    assert(msg2.magicByte === kafkaMsg2.magic)
+    assert(msg2.attributes === kafkaMsg2.attributes)
+    assert(msg2.key === None)
+    assert(msg2.value.toString(utf8) === "value2")
   }
-
-  behavior of "Spec.encodeMessageSet"
-
-  behavior of "Spec.decodeMessageSet"
 
 }
